f25aad25b6bebe6c61517ece3f7f0e20dd302752
Lee Rhodes <lrhodes@yahoo-inc.com>
2016-08-10 14:43:43 -0700

    Rewrite of MemoryPerformance

--- src/main/java/com/yahoo/sketches/memory/MemoryPerformance.java
+++ src/main/java/com/yahoo/sketches/memory/MemoryPerformance.java
@@ -7,226 +7,177 @@ package com.yahoo.sketches.memory;
 
 import static com.yahoo.sketches.Util.zeroPad;
 import static com.yahoo.sketches.memory.UnsafeUtil.unsafe;
-import static java.lang.Math.floor;
 import static java.lang.Math.pow;
 
 public final class MemoryPerformance {
-  private int arrLongs_;    //# entries in array used for heap R/W
-  private int lgMaxTrials_; //maximum X-axis
-  private int ppo_;         //points per octave
-  private int lastGI_;      //last generating index
-  private long[] array_;    //array used for heap R/W
-  private long checkSum_;
+  private int arrLongs_;    //# entries in array
+  private int lgMinTrials_; //minimum # of trials
+  private int lgMaxTrials_; //maximum # of trials
+  private int lgMinLongs_;  //minimum array longs
+  private int lgMaxLongs_;  //maximum array longs
+  private int ppo_;         //points per octave of arrLongs (x-axis)
+  private int minGI_;     //min generating index
+  private int maxGI_;     //max generating index
+  private int lgMaxOps_;    //lgMaxLongs_ + lgMinTrials_
   private long address_ = 0; //used for unsafe
   
   public MemoryPerformance() {
     //Configure
-    arrLongs_ = 1 << 14; //16K entries
-    lgMaxTrials_ = 14; //16K trials
-    ppo_ = 4;
+    lgMinTrials_ = 6;  //6
+    lgMaxTrials_ = 20; //20
+    lgMinLongs_ = 5;   //5
+    lgMaxLongs_ = 26;  //26
+    ppo_ = 4; //4
     //Compute
-    lastGI_ = ppo_ * lgMaxTrials_;
-    array_ = new long[arrLongs_];
-    checkSum_ = computeCorrectChecksum();
+    maxGI_ = ppo_ * lgMaxLongs_;
+    minGI_ = ppo_ * lgMinLongs_;
+    lgMaxOps_ = lgMaxLongs_ + lgMinTrials_;
   }
   
-  private final long computeCorrectChecksum() {
-    long sum = 0L;
-    for (int i=1; i<=arrLongs_; i++) { sum += i; }
-    return sum;
+  private class Point {
+    int gi;
+    int arrLongs;
+    int trials;
+    
+    Point(int gi, int arrLongs, int trials) {
+      this.gi = gi;
+      this.arrLongs = arrLongs;
+      this.trials = trials;
+    }
+  }
+  
+  private Point getNextPoint(Point p) {
+    int lastArrLongs = (int)pow(2.0, (double)p.gi/ppo_);
+    int nextArrLongs;
+    double logArrLongs;
+    do {
+      logArrLongs = (++p.gi)/(double)ppo_;
+      if (p.gi > maxGI_) return null;
+      nextArrLongs = (int)pow(2.0, logArrLongs);
+    } while (nextArrLongs <= lastArrLongs);
+    p.arrLongs = lastArrLongs = nextArrLongs;
+    //compute trials
+    double logTrials = Math.min(lgMaxOps_- logArrLongs, lgMaxTrials_);
+    p.trials = (int)pow(2.0, logTrials);
+    return p;
   }
   
   /*************************************/
   // ON-HEAP
   /*************************************/
   
-  private void loadHeapArray() {
-    for (int i=0; i<arrLongs_; i++) { array_[i] = i+1; }
-  }
-  
-  private void clearHeapArray() {
-    for (int i=0; i<arrLongs_; i++) { array_[i] = 0L; }
-  }
-  /*************************************/
-  
-  private void testReadHeapArrayByIndex() {
-    loadHeapArray();
-    println("Trials\tTrial_nS\tOp_nS\t#Ops");
-    int lastTrials = 0;
-    for (int gi = 0; gi <= lastGI_; gi++) {
-      int trials = (int)floor(pow(2.0, (double)gi/ppo_));
-      if (trials == lastTrials) continue;
-      lastTrials = trials;
-      long sumTrialsTime_nS = 0;
-      for (int t=0; t<trials; t++) { 
-        //run a single trial
-        sumTrialsTime_nS += readHeapArrayByIndexTrial(array_, checkSum_);
-      }
-      long trial_nS = (long)((double)sumTrialsTime_nS/trials);
-      double numOps = (double)trials * arrLongs_;
-      String numOpsStr = String.format("%.0f", numOps);
-      double singleOp_nS = sumTrialsTime_nS/numOps;
-      String readTimeStr = String.format("%6.3f", singleOp_nS);
-      //output summary of trials per gi
-      println(trials+"\t"+trial_nS+"\t"+readTimeStr+"\t"+numOpsStr);
-    }
-  }
-  
-  private static final long readHeapArrayByIndexTrial(long[] array, long checkSum) {
-    long trialSum = 0;
-    int size = array.length;
-      //Timing interval for a single trial
-      long startTime_nS = System.nanoTime();
-      for (int i=0; i<size; i++) { trialSum += array[i]; }
-      long trialTime_nS = System.nanoTime() - startTime_nS;
+  private void testHeapArrayByIndex() {
+    println("LgLongs\tLongs\tTrials\t#Ops\tAvgRTrial_nS\tAvgROp_nS\tAvgWTrial_nS\tAvgWOp_nS");
+    Point p = new Point(minGI_-1, 1<<lgMinLongs_, 1<<lgMaxTrials_); //just below the start
+    while ((p = getNextPoint(p)) != null) {
+      long[] array = new long[p.arrLongs];
       
-    if (trialSum != checkSum) throw new IllegalStateException("Bad checksum: "+trialSum+" != "+checkSum);
-    return trialTime_nS;
-  }
-  
-  /*************************************/
-  
-  private void testWriteHeapArrayByIndex() {
-    println("Trials\tTrial_nS\tOp_nS\t#Ops");
-    clearHeapArray();
-    int lastTrials = 0;
-    for (int gi = 0; gi <= lastGI_; gi++) {
-      int trials = (int)floor(pow(2.0, (double)gi/ppo_));
-      if (trials == lastTrials) continue;
-      lastTrials = trials;
-      long sumTrialsTime_nS = 0;
-      for (int t=0; t<trials; t++) { 
-        //run a single trial
-        sumTrialsTime_nS += writeHeapArrayByIndexTrial(array_, checkSum_);
+      //Do all write trials at this array size point
+      long sumWriteTrials_nS = 0;
+      for (int t=0; t<p.trials; t++) {
+        sumWriteTrials_nS += trial_HeapArrayByIndex(array, false); //a single trial write
       }
-      long trial_nS = (long)((double)sumTrialsTime_nS/trials);
-      double numOps = (double)trials * arrLongs_;
-      String numOpsStr = String.format("%.0f", numOps);
-      double singleOp_nS = sumTrialsTime_nS/numOps;
-      String readTimeStr = String.format("%6.3f", singleOp_nS);
-      //output summary of trials per gi
-      println(trials+"\t"+trial_nS+"\t"+readTimeStr+"\t"+numOpsStr);
+      //Do all read trials at this array size point
+      long sumReadTrials_nS  = 0;
+      for (int t=0; t<p.trials; t++) {
+        sumReadTrials_nS += trial_HeapArrayByIndex(array, true); //a single trial read
+      }
+      //Print Results
+      printOut(p.gi, sumReadTrials_nS, sumWriteTrials_nS, p.trials, p.arrLongs);
     }
   }
   
-  private static final long writeHeapArrayByIndexTrial(long[] array, long checkSum) {
-    int size = array.length;
+  //Must do writes first
+  private static final long trial_HeapArrayByIndex(long[] array, boolean read) {
+    int arrLongs = array.length;
+    long checkSum = (arrLongs * (arrLongs - 1L)) /2L;
+    long startTime_nS, stopTime_nS; 
+    if (read) {
       //Timing interval for a single trial
-      long startTime_nS = System.nanoTime();
-      for (int i=0; i<size; i++) { array[i] = i+1; }
-      long trialTime_nS = System.nanoTime() - startTime_nS;
-    
-    readHeapArrayByIndexTrial(array, checkSum); //does the checksum
-    return trialTime_nS;
+      long trialSum = 0;
+      startTime_nS = System.nanoTime();
+      for (int i=0; i<arrLongs; i++) { trialSum += array[i]; }
+      stopTime_nS = System.nanoTime();
+      if (trialSum != checkSum) {
+        throw new IllegalStateException("Bad checksum: "+trialSum+" != "+checkSum);
+      }
+    } else { //write
+      startTime_nS = System.nanoTime();
+      for (int i=0; i<arrLongs; i++) { array[i] = i; }
+      stopTime_nS = System.nanoTime();
+    }
+    return stopTime_nS - startTime_nS;
   }
   
   /*************************************/
   //  OFFHEAP
   /*************************************/
   
-  private void allocateMemory() {
-    if (address_ == 0) {
-      address_ = unsafe.allocateMemory(arrLongs_ << 3);
-    }
-  }
-  
-  private void loadNativeMemory() {
-    allocateMemory();
-    for (long i=0; i<arrLongs_; i++) {
-      unsafe.putLong(address_ + (i << 3), i+1);
-    }
-  }
-  
-  private void clearNativeMemory() {
-    if (address_ == 0) {
-      unsafe.setMemory(null, address_, arrLongs_ << 3, (byte) 0);
-    }
-  }
-  
-  /*************************************/
-  
-  private void testReadNativeArrayByUnsafe() {
-    loadNativeMemory();
-    println("Trials\tTrial_nS\tOp_nS\t#Ops");
-    int lastTrials = 0;
-    for (int gi = 0; gi <= lastGI_; gi++) {
-      int trials = (int)floor(pow(2.0, (double)gi/ppo_));
-      if (trials == lastTrials) continue;
-      lastTrials = trials;
-      long sumTrialsTime_nS = 0;
-      for (int t=0; t<trials; t++) { 
-        //run a single trial
-        sumTrialsTime_nS += readNativeArrayByUnsafeTrial(address_, arrLongs_, checkSum_);
-      }
-      long trial_nS = (long)((double)sumTrialsTime_nS/trials);
-      double numOps = (double)trials * arrLongs_;
-      String numOpsStr = String.format("%.0f", numOps);
-      double singleOp_nS = sumTrialsTime_nS/numOps;
-      String readTimeStr = String.format("%6.3f", singleOp_nS);
-      //output summary of trials per gi
-      println(trials+"\t"+trial_nS+"\t"+readTimeStr+"\t"+numOpsStr);
-    }
-    unsafe.freeMemory(address_);
+  private void testNativeArrayByUnsafe() {
+    println("LgLongs\tLongs\tTrials\t#Ops\tAvgRTrial_nS\tAvgROp_nS\tAvgWTrial_nS\tAvgWOp_nS");
+    Point p = new Point(minGI_-1, 1<<lgMinLongs_, 1<<lgMaxTrials_); //just below the start
     address_ = 0L;
-  }
-  
-  //Includes 4 variations of the for-loop.
-  @SuppressWarnings("unused")
-  private static final long readNativeArrayByUnsafeTrial(long address, int arrLongs, long checkSum) {
-    long trialSum = 0;
-    long capBytes = arrLongs << 3;
-      //Timing interval for a single trial
-      long startTime_nS = System.nanoTime(); //time to execute a trial
-      for (int i=0; i<arrLongs; i++)  { trialSum += unsafe.getLong(address + (i << 3)); } //fastest
-      //for (int i=0; i<arrLongs; i++)  { trialSum += unsafe.getLong(address +  i * 8); } //faster
-      //for (int i=arrLongs; i-- >0; )  { trialSum += unsafe.getLong(address + (i << 3)); }//slower
-      //for (int i=0; i<capBytes; i+=8) { trialSum += unsafe.getLong(address + i); } //slowest
-      long trialTime_nS = System.nanoTime() - startTime_nS;
-    
-    if (trialSum != checkSum) throw new IllegalStateException("Bad checksum: "+trialSum+" != "+checkSum);
-    return trialTime_nS;
-  }
-  
-  /*************************************/
-  
-  private void testWriteNativeArrayByUnsafe() {
-    allocateMemory();
-    clearNativeMemory();
-    println("Trials\tTrial_nS\tOp_nS\t#Ops");
-    int lastTrials = 0;
-    for (int gi = 0; gi <= lastGI_; gi++) {
-      int trials = (int)floor(pow(2.0, (double)gi/ppo_));
-      if (trials == lastTrials) continue;
-      lastTrials = trials;
-      long sumTrialsTime_nS = 0;
-      for (int t=0; t<trials; t++) { 
-        //run a single trial
-        sumTrialsTime_nS += writeNativeArrayByUnsafeTrial(address_, arrLongs_, checkSum_);
+    while ((p = getNextPoint(p)) != null) {
+      address_ = unsafe.allocateMemory(p.arrLongs << 3);
+      //Do all write trials at this array size point
+      long sumWriteTrials_nS = 0;
+      for (int t=0; t<p.trials; t++) { //do writes first
+        sumWriteTrials_nS += trial_NativeArrayByUnsafe(address_, p.arrLongs, false); //a single trial write
       }
-      long trial_nS = (long)((double)sumTrialsTime_nS/trials);
-      double numOps = (double)trials * arrLongs_;
-      String numOpsStr = String.format("%.0f", numOps);
-      double singleOp_nS = sumTrialsTime_nS/numOps;
-      String readTimeStr = String.format("%6.3f", singleOp_nS);
-      //output summary of trials per gi
-      println(trials+"\t"+trial_nS+"\t"+readTimeStr+"\t"+numOpsStr);
+      //Do all read trials at this array size point
+      long sumReadTrials_nS  = 0;
+      for (int t=0; t<p.trials; t++) {
+        sumReadTrials_nS += trial_NativeArrayByUnsafe(address_, p.arrLongs, true); //a single trial read
+      }
+      //Print Results
+      printOut(p.gi, sumReadTrials_nS, sumWriteTrials_nS, p.trials, p.arrLongs);
+      freeMemory();
     }
-    unsafe.freeMemory(address_);
-    address_ = 0L;
-    
   }
   
-  private static final long writeNativeArrayByUnsafeTrial(long address, int arrLongs, long checkSum) {
+  //Must do writes first
+  private static final long trial_NativeArrayByUnsafe(long address, int arrLongs, boolean read) {
+    long checkSum = (arrLongs * (arrLongs - 1L)) /2L;
+    long startTime_nS, stopTime_nS; 
+    if (read) {
       //Timing interval for a single trial
-      long startTime_nS = System.nanoTime();
-      for (int i=0; i<arrLongs; i++)  { unsafe.putLong(address + (i << 3), i+1); }
-      long trialTime_nS = System.nanoTime() - startTime_nS;
-    
-    readNativeArrayByUnsafeTrial(address, arrLongs, checkSum); //does the checksum
-    return trialTime_nS;
+      long trialSum = 0;
+      startTime_nS = System.nanoTime();
+      for (int i=0; i<arrLongs; i++) { trialSum += unsafe.getLong(address + (i << 3)); }
+      stopTime_nS = System.nanoTime();
+      if (trialSum != checkSum) {
+        if (address != 0) { unsafe.freeMemory(address); }
+        throw new IllegalStateException("Bad checksum: "+trialSum+" != "+checkSum);
+      }
+    } else { //write
+      startTime_nS = System.nanoTime();
+      for (int i=0; i<arrLongs; i++) { unsafe.putLong(address + (i << 3), i); }
+      stopTime_nS = System.nanoTime();
+    }
+    return stopTime_nS - startTime_nS;
   }
   
   /*************************************/
+  
+  private void printOut(int gi, long sumReadTrials_nS, long sumWriteTrials_nS, int trials, int arrLongs) {
+    double logArrLongs = gi/(double)ppo_;
+    double rTrial_nS = (double)sumReadTrials_nS/trials;
+    double wTrial_nS = (double)sumWriteTrials_nS/trials;
+    long numOps = (long)((double)trials * arrLongs);
+    double rOp_nS = sumReadTrials_nS/(double)numOps;
+    double wOp_nS = sumWriteTrials_nS/(double)numOps;
+    //Print
+    String out = String.format("%6.3f\t%d\t%d\t%d\t%.1f\t%8.3f\t%.1f\t%8.3f", 
+        logArrLongs, arrLongs, trials, numOps, rTrial_nS, rOp_nS, wTrial_nS, wOp_nS);
+    println(out);
+  }
+  
+  private final void freeMemory() {
+    if (address_ != 0) {
+      unsafe.freeMemory(address_);
+      address_ = 0L;
+    }
+  }
   
   /**
    * If the JVM calls this method and a "freeMemory() has not been called" a <i>System.err</i>
@@ -265,14 +216,10 @@ public final class MemoryPerformance {
   //MAIN
   public static void main(String[] args) {
     MemoryPerformance test = new MemoryPerformance();
-    println("Test Read Heap Array By Index");
-    test.testReadHeapArrayByIndex();
-    println("\nTest Write Heap Array By Index");
-    test.testWriteHeapArrayByIndex();
-    println("\nTest Read Native Array By Unsafe");
-    test.testReadNativeArrayByUnsafe();
-    println("\nTest Write Native Array By Unsafe");
-    test.testWriteNativeArrayByUnsafe();
+    println("Test Heap Array By Index");
+    test.testHeapArrayByIndex();
+    println("\nTest Native Memory By Unsafe");
+    test.testNativeArrayByUnsafe();
   }
   
 }
